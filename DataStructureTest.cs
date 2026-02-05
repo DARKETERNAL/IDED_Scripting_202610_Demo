a998940f496405b6cdf90c87944328e0a6a1e903
andres <tomailsandstuff@gmail.com>
2026-02-04 21:30:23 -0500

    Added stack sample

--- DataStructureTest.cs
+++ DataStructureTest.cs
@@ -4,70 +4,21 @@
     {
         public static void Main(string[] args)
         {
-            int[] numbers = [-18, 422, 37, 14, 50]; // Hypothetical array literal syntax in C# 13.0
-            ////int sum = numbers.Length;
+            Stack<string> stack = new Stack<string>();
 
-            //int[] numbersB = new int[5];
-            ////int[] numberB = new int[5] { -18, 422, 37, 14, 50 };
+            Console.WriteLine($"Stack elements:{stack.Count}");
 
-            List<int> numberList = new() { -18, 422, 37, 14, 50 };
-            List<int> numberListB = new(); //5? //0?
-            List<int> numberListC = new List<int>(5); // 5
+            stack.Push("First");
+            stack.Push("Second");
+            stack.Push("Third");
+            Console.WriteLine($"Stack elements:{stack.Count}");
 
-            //Console.WriteLine($"numberList[3]:{numberList[3]}");
+            Console.WriteLine(stack.Peek());
 
-            PrintListElements(numberListB);
-            Console.WriteLine("----");
-            numberListB.Add(54762398);
-            Console.WriteLine("----");
-            PrintListElements(numberListB);
-
-            Console.WriteLine("----");
-            Console.WriteLine("----");
-
-            PrintListElements(numberList);
-            Console.WriteLine("----");
-            numberList.RemoveAt(2);
-            Console.WriteLine("----");
-            PrintListElements(numberList);
-
-            Console.WriteLine("----");
-            numberList.Insert(4, 123456);
-            Console.WriteLine("----");
-            PrintListElements(numberList);
-
-            Console.WriteLine("----");
-            numberList.Add(987654);
-            Console.WriteLine("----");
-            PrintListElements(numberList);
-
-            Console.WriteLine("----");
-            numberList[1] = 0;
-            Console.WriteLine("----");
-            PrintListElements(numberList);
-
-            Console.WriteLine("----");
-
-            for (int i = 0; i < 200; i++)
+            foreach (var item in stack)
             {
-                numberListC.Add(i * 111);
+                Console.WriteLine(item);
             }
-
-            Console.WriteLine("----");
-            PrintListElements(numberListC);
-        }
-
-        private static int PrintListElements(List<int> list)
-        {
-            int index = 0;
-
-            while (index < list.Count)
-            {
-                Console.WriteLine($"numberList[{index}]:{list[index]}");
-                index++;
-            }
-
-            return index;
         }
     }
 }
