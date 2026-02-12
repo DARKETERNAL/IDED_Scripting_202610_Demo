fbf42cf3f9e5311e29f929ba76f41ad1aee3936b
andres <tomailsandstuff@gmail.com>
2026-02-11 21:25:37 -0500

    Added queue sample

--- DataStructureTest.cs
+++ DataStructureTest.cs
@@ -2,23 +2,78 @@
 {
     public class DataStructureTest
     {
+        public static string FindInQueue(Queue<string> queue, string expectedValue)
+        {
+            string result = null;
+            bool foundString = false;
+
+            while (!foundString && queue.Count > 0)
+            {
+                foundString = queue.Peek().Equals(expectedValue);
+
+                if (foundString)
+                {
+                    result = queue.Peek();
+                }
+                else
+                {
+                    queue.Dequeue();
+                }
+            }
+
+            return result;
+        }
+
         public static void Main(string[] args)
         {
-            Stack<string> stack = new Stack<string>();
+            Queue<string> queue = new Queue<string>();
 
-            Console.WriteLine($"Stack elements:{stack.Count}");
+            Console.WriteLine($"Queue elements:{queue.Count}");
 
-            stack.Push("First");
-            stack.Push("Second");
-            stack.Push("Third");
-            Console.WriteLine($"Stack elements:{stack.Count}");
+            queue.Enqueue("First");
+            queue.Enqueue("Second");
+            queue.Enqueue("Third");
+            Console.WriteLine($"Queue elements:{queue.Count}");
 
-            Console.WriteLine(stack.Peek());
+            //Console.WriteLine(queue.Peek());
 
-            foreach (var item in stack)
+            //while (queue.Count > 0)
+            //{
+            //    Console.WriteLine(queue.Dequeue());
+            //    Console.WriteLine($"Queue elements:{queue.Count}");
+            //}
+
+            //for (int i = 0; i < queue.Count;)
+            //for (int i = queue.Count; i > 0; i = queue.Count)
+            //{
+            //    Console.WriteLine(queue.Dequeue());
+            //    Console.WriteLine($"Queue elements:{queue.Count}");
+            //}
+
+            //do
+            //{
+            //    Console.WriteLine(queue.Dequeue());
+            //    Console.WriteLine($"Queue elements:{queue.Count}");
+            //}
+            //while (queue.Count > 0);
+
+            //int count = queue.Count;
+
+            //for (int i = 0; i < count; i++)
+            //{
+            //    Console.WriteLine(queue.Dequeue());
+            //    Console.WriteLine($"Queue elements:{queue.Count}");
+            //}
+
+            string expectedValue = "Fourth";
+            string result = FindInQueue(queue, expectedValue);
+
+            if (string.IsNullOrEmpty(result))
             {
-                Console.WriteLine(item);
+                result = "Not found";
             }
+
+            Console.WriteLine($"Expected value: {expectedValue}, Result: {result}");
         }
     }
 }
